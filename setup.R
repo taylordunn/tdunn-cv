feaf0929a6c1d24a7f6af0eeac78c766b7c67fc3
taylordunn <t.dunn19@gmail.com>
2022-01-09 13:52:37 -0400

    Edit the link to PDF

--- setup.R
+++ setup.R
@@ -1,7 +1,7 @@
 datadrivencv::use_datadriven_cv(
   full_name = "Taylor Dunn",
   data_location = "https://docs.google.com/spreadsheets/d/1TeGj9u65lTVJU4XvWwvyxkMo3RUuwpkithV-7rIuqZo",
-  pdf_location = "https://github.com/taylordunn/tdunn-cv/raw/main/tdunn-cv.pdf",
+  pdf_location = "https://github.com/taylordunn/tdunn-cv/raw/main/cv.pdf",
   html_location = "tdunn.ca/cv",
   source_location = "https://github.com/taylordunn/tdunn-cv",
   use_network_logo = FALSE
